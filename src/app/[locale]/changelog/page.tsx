8c6432ed16e156b222b5994273705d94b079c743
angel <angel.rodriguez3rm@gmail.com>
2026-01-30 18:28:19 +0100

    privacy policy #2

--- src/app/[locale]/changelog/page.tsx
+++ src/app/[locale]/changelog/page.tsx
@@ -22,7 +22,7 @@ export default async function ChangelogPage({ params }: Props) {
         <div className="mx-auto flex min-h-screen max-w-3xl flex-col px-4 pb-16 pt-8 sm:px-8 sm:pt-10">
           <Header />
 
-          <section className="mt-8 flex flex-1 flex-col gap-6 pt-6 sm:mt-14 sm:gap-10 sm:pt-10">
+          <section className="mt-8 flex flex-1 flex-col gap-6 pt-20 sm:mt-14 sm:gap-10 sm:pt-10">
             <Timeline />
           </section>
         </div>
